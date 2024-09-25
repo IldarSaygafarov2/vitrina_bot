5e1cc6094cd6a2a9f4e5400ab34b36c4bcff229f
IldarSaygafarov2 <sayildar17@gmail.com>
2024-09-26 01:40:45 +0500

    added fixes

--- handlers/rg.py
+++ handlers/rg.py
@@ -11,12 +11,14 @@ from states.custom_states import RGProcessState
 router = Router(name='rg_user')
 
 
-@router.message(CommandStart(), F.func(lambda msg: user_manager.is_user_rg(msg.from_user.username)))
+@router.message(CommandStart(),
+                F.func(lambda msg: user_manager.is_user_rg(msg.from_user.username)))
 async def rg_start(message: types.Message):
     await message.answer(f'Привет, Руководитель группы', reply_markup=reply.rg_start_kb())
 
 
-@router.message(F.text == 'Список риелторов')
+@router.message(F.text == 'Список риелторов',
+                F.func(lambda msg: user_manager.is_user_rg(msg.from_user.username)))
 async def rg_realtors(message: types.Message):
     res = api_manager.user_service.get_all_users(params={'user_type': 'group_director'})
     await message.answer('Список риелторов', reply_markup=callback.realtors_kb(res))
@@ -25,8 +27,6 @@ async def rg_realtors(message: types.Message):
 @router.callback_query(F.data.contains('realtor'))
 async def rg_realtors_ads(callback_query: types.CallbackQuery, state: FSMContext):
     _, realtor_username, user_id = callback_query.data.split('-')
-    # objects: list[dict] = api_manager.user_service.get_user_advertisements(user_id=int(user_id))
-    await state.set_state(RGProcessState.show_moderated)
     realtor_data = {
         'username': realtor_username,
         'realtor_id': user_id
@@ -35,42 +35,9 @@ async def rg_realtors_ads(callback_query: types.CallbackQuery, state: FSMContext
     await callback_query.message.answer(f'Какие объявления показать', reply_markup=reply.ad_moderated_kb())
 
 
-#     for obj in objects:
-#         print(obj)
-#
-#         t = f'{html.bold('Кол-во комнат: ')}от {html.italic(obj.get('rooms_qty_from'))} до {html.italic(obj.get('rooms_qty_to'))}'
-#         t2 = f'\n{html.bold("Год постройки: ")}{html.italic(obj.get('creation_year'))}' if obj.get('creation_year') else ''
-#         #
-#         msg = f'''
-# {html.bold('Заголовок: ')}
-# {html.italic(obj.get('name'))}
-# {html.bold('Тип объявления: ')}
-# {html.italic(obj.get('operation_type'))}
-# {html.bold('Описание: ')}
-# {html.italic(obj.get('description'))}
-# {html.bold('Район: ')}{html.italic(obj.get('district').get('name'))}
-# {html.bold('Адрес: ')}{html.italic(obj.get('address'))}
-# {html.bold('Категория недвижимости: ')}{html.italic(obj.get('category').get('name'))}
-# {html.bold('Тип недвижимости: ')}{html.italic(obj.get('property_type'))}{t2}
-# {html.bold('Цена: ')}{html.italic(obj.get('price'))}
-# {t if not obj.get('is_studio') else f'{html.bold("Кол-во комнат: ")} Студия'}
-# {html.bold('Квадратура: ')}от {html.italic(obj.get('quadrature_from'))} до {html.italic(obj.get('quadrature_to'))}
-# {html.bold('Этаж: ')}от {html.italic(obj.get('floor_from'))} до {html.italic(obj.get('floor_to'))}
-# {html.bold('Ремонт: ')}{html.italic(obj.get('repair_type'))}
-# '''
-#         # await callback_query.message.answer(msg)
-#         photos = []
-#         for idx, photo in enumerate(obj.get('gallery')):
-#             photo_obj = types.InputMediaPhoto(media=photo['photo'])
-#             photos.append(photo_obj)
-#
-#         await callback_query.message.answer_media_group(photos)
-
-#
-# await callback_query.message.answer_media_group(media)
-
 @router.message(F.text == 'Проверенные')
 async def get_moderated_ads(message: types.Message, state: FSMContext):
+    print('assss')
     data = await state.get_data()
     user_id = int(data['realtor_data']['realtor_id'])
     username = data['realtor_data']['username']
@@ -87,6 +54,7 @@ async def get_moderated_ads(message: types.Message, state: FSMContext):
 
 @router.message(F.text == 'Непроверенные')
 async def get_moderated_ads(message: types.Message, state: FSMContext):
+    print('assss2222')
     data = await state.get_data()
     user_id = int(data['realtor_data']['realtor_id'])
     username = data['realtor_data']['username']
