3b7fbba2ffe5353de79425dd1deaa196e809da5b
IldarSaygafarov2 <sayildar17@gmail.com>
2024-10-31 10:42:03 +0500

    added fixes

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,14 +1,14 @@
 from .advertisement import router as advertisement_router
 from .advertisement_update import router as advertisement_editing_router
 from .group_director import router as group_director_router
-
+from .simple_user import router as simple_user_router
 # from .simple_user import router as user_router
 
 routers_list = [
     advertisement_router,
     advertisement_editing_router,
     group_director_router,
-    # user_router
+    simple_user_router
 ]
 
 __all__ = [
--- handlers/simple_user.py
+++ handlers/simple_user.py
@@ -1,36 +1,9 @@
-# from aiogram import types, Router
-# from aiogram.filters import Command
-#
-# from keyboards import reply, callback
-# from services.api import api_manager
-#
-# router = Router(name='simple_user')
-#
-#
-# @router.message(Command(commands=['start']))
-# async def simple_user(message: types.Message):
-#     current_user_username = message.from_user.username
-#     realtors = api_manager.user_type.get_all_users_by_user_type(user_type='realtor')
-#     if not realtors:
-#         return await message.answer('Вас приветствует бот Vitrina')
-#     realtors_usernames = list(map(lambda realtor: realtor['username'], realtors))
-#     if current_user_username in realtors_usernames:
-#         return await message.answer(f'Привет, риелтор: {current_user_username}',
-#                                     reply_markup=reply.start_kb())
-#
-#     group_directors = api_manager.user_type.get_all_users_by_user_type(user_type='group_director')
-#     if not group_directors:
-#         return await message.answer('Вас приветствует бот Vitrina')
-#     group_directors_usernames = list(map(lambda group_director: group_director['username'], group_directors))
-#     if current_user_username in group_directors_usernames:
-#         return await message.answer(f'Привет, руководитель группы: {current_user_username}',
-#                                     reply_markup=callback.group_director_kb())
-#
-#     simple_admins = api_manager.user_type.get_all_users_by_user_type(user_type='simple_admin')
-#     if not simple_admins:
-#         return await message.answer('Вас приветствует бот Vitrina')
-#     simple_admins_usernames = list(map(lambda simple_admin: simple_admin['username'], simple_admins))
-#     if current_user_username in simple_admins_usernames:
-#         return await message.answer(f'Привет, админ: {current_user_username}', )
-#
-#     await message.answer(f'Вас приветствует бот Vitrina')
+from aiogram import types, Router
+from aiogram.filters import CommandStart
+
+router = Router(name='simple_user')
+
+
+@router.message(CommandStart())
+async def simple_user_start(message: types.Message):
+    await message.answer(f'Добро пожаловать в бот Vitrina')
